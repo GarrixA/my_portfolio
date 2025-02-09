cb7f1173e709605b92860ff3e48a21c95a88781c
GarrixA <aphrogarrix10@gmail.com>
2025-02-09 23:04:25 +0200

    fix-blogs-data

--- src/utils/blogs.ts
+++ src/utils/blogs.ts
@@ -7,45 +7,79 @@ import blogImage5 from "@/public/blogImage5.png";
 export const myBlogs = [
   {
     id: 1,
-    title: "My journey at ATLP",
-    description: `The Andela Technical Leadership Program is a comprehensive 9-month course designed to equip individuals with both technical and soft skills necessary for success as software engineers.
-
- 
-
-I decided to join this program for several reasons. Firstly, I had been hearing about Andela since 2021, and the positive feedback from those who worked there intrigued me. People often spoke about the benefits, such as the flexibility to work from home, the absence of the daily struggle of commuting, and the financial stability it provided. These aspects appealed to me greatly, especially since I was tired of the daily grind of commuting to my previous job, which required taking two buses. Additionally, my interests in imagination and coding led me to believe that pursuing a career in software engineering would align well with my passions.
-
- 
-
-Upon joining the program, I quickly realized the potential it held for advancing my career. Through rigorous training in JavaScript, data structures, algorithms, Node.js, TypeScript, and CI/CD, I have enhanced my technical skills significantly. Moreover, the program's emphasis on communication and collaboration has helped me develop strong interpersonal skills, which are crucial in the tech industry. Understanding how databases work and integrating backend and frontend components has given me a holistic view of the software development lifecycle, further solidifying my expertise in this field.
-    `,
+    title: "Good life must last longer",
+    description: `  A longer lifespan allows for more time to develop skills, pursue passions, and achieve personal goals, leading to a richer and more fulfilling life. 
+Stronger relationships:
+Extended time with loved ones allows for deeper connections and more meaningful experiences within families and friendships. 
+Social contribution:
+Living longer can enable individuals to contribute to their communities for a longer period, sharing their wisdom and experience with younger generations. 
+Enhanced resilience:
+Facing challenges and overcoming obstacles over a longer lifespan can build emotional strength and resilience. 
+Potential for continued learning:
+A longer life allows for ongoing learning and exploration of new interests, keeping the mind active and engaged. 
+However, it's important to note that simply living longer doesn't guarantee a good life; maintaining a healthy lifestyle, having a sense of purpose, and actively engaging with life are crucial factors for a fulfilling existence regardless of lifespan.  `,
     image: blogImage1.src,
   },
   {
     id: 2,
     title: "My journey at ATLP",
-    description:
-      "The Andela Technical Leadership Program is a comprehensive 9-month course",
+    description: `The Andela Technical Leadership Program is a comprehensive 9-month course designed to equip individuals with both technical and soft skills necessary for success as software engineers.
+I decided to join this program for several reasons. Firstly, I had been hearing about Andela since 2021, and the positive feedback from those who worked there intrigued me. People often spoke about the benefits, such as the flexibility to work from home, the absence of the daily struggle of commuting, and the financial stability it provided. These aspects appealed to me greatly, especially since I was tired of the daily grind of commuting to my previous job, which required taking two buses. Additionally, my interests in imagination and coding led me to believe that pursuing a career in software engineering would align well with my passions.
+Upon joining the program, I quickly realized the potential it held for advancing my career. Through rigorous training in JavaScript, data structures, algorithms, Node.js, TypeScript, and CI/CD, I have enhanced my technical skills significantly. Moreover, the program's emphasis on communication and collaboration has helped me develop strong interpersonal skills, which are crucial in the tech industry. Understanding how databases work and integrating backend and frontend components has given me a holistic view of the software development lifecycle, further solidifying my expertise in this field.
+ `,
     image: blogImage2.src,
   },
   {
     id: 3,
-    title: "My journey at ATLP",
-    description:
-      "Chelsea Football Club  is an English professional football club based in Fulham, West London. Founded in 1905, the team play their home games at Stamford Bridge. The club competes in the Premier League, the top division of English football. It won its first major honour, the League championship, in 1955.",
+    title: "Chelsea Football",
+    description: `A former borough in Greater London, England. It is now part of Kensington and Chelsea, which is the smallest borough in London. 
+The name Chelsea comes from the Old English words cealc meaning chalk or limestone, and hythe meaning port or landing place. 
+The Manor of Chelsea was given to the Abbot and Convent of Westminster by Thurstan, the governor of the King's Palace during the reign of Edward the Confessor. 
+Chelsea, Massachusetts 
+A city in eastern Massachusetts, near Boston.
+Chelsea Football Club
+A professional football club based in Fulham, West London, England. 
+The club was founded in 1905 and plays its home games at Stamford Bridge. 
+The club's nickname is "The Blues". 
+Chelsea has won the FA Cup, the League championship, the Cup Winners' Cup, the UEFA Super Cup, and the Champions League. 
+Chelsea boot 
+A type of boot that was popularized by the Beatles in the 1960s.
+The Beatles commissioned four pairs of Chelsea boots with Cuban heels from the theatrical and ballet shoe maker Anello & Davide.
+Chelsea, a female name 
+A female name that comes from the Old English words cealc and hythe.`,
     image: blogImage3.src,
   },
   {
     id: 4,
-    title: "My journey at ATLP",
-    description:
-      "The Andela Technical Leadership Program is a comprehensive 9-month course",
+    title: "Amahoro Stadium",
+    description: `Amahoro National Stadium is a multi-purpose stadium in Kigali, Rwanda. It's the biggest stadium in the country and hosts football matches, concerts, and other events. The stadium's name translates to "Peace Stadium" in Kinyarwanda. 
+      Construction began in 1984 and finished in 1989 with Chinese aid 
+The stadium was renovated between 2011–2016 and 2022–2024 
+The stadium was a UN Protected Site during the 1994 Rwandan genocide, providing shelter to refugees 
+Features:
+The stadium has a hybrid grass pitch 
+It has a VIP lounge, bars, restaurants, and shops 
+The stadium has a Video Assistant Referee (VAR) room, match officials' rooms, and a players' medical room 
+The stadium has an athletics track, football and rugby pitches, and a tennis field 
+Events:
+The stadium hosts matches for the Rwanda national football team, Armée Patriotique Rwandaise F.C., and Rayon Sports F.C. 
+The stadium has hosted concerts, graduation ceremonies, and other public events 
+The stadium has been approved to host international football competitions by FIFA and the Confederation of African Football (CAF) `,
     image: blogImage4.src,
   },
   {
     id: 5,
-    title: "My journey at ATLP",
-    description:
-      "The Andela Technical Leadership Program is a comprehensive 9-month course",
+    title: "African Dress Code",
+    description: `A "dress code" in school refers to a set of rules that specify what clothing and appearance is considered acceptable for students to wear while attending school, usually outlining details like appropriate clothing types, length, and style, essentially dictating how students should present themselves at school. 
+Key points about school dress codes:
+Purpose:
+To promote a focused learning environment, maintain a positive school image, and sometimes to minimize distractions related to clothing choices. 
+Components:
+Dress codes can include guidelines on shirt styles, pants length, skirt length, footwear, accessories, hairstyles, and overall neatness. 
+Variations:
+Some schools might have a strict dress code with uniforms, while others may have a more relaxed "casual" dress code with specific restrictions. 
+Potential concerns:
+Some argue that dress codes can limit personal expression and may disproportionately affect certain genders or cultural groups`,
     image: blogImage5.src,
   },
 ];
